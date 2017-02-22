d7728579a7af2b0df8b15038c7b278f9c4dbc4a9
Noel Parisi <NoelParisi@Noels-MacBook-Pro.local>
2017-02-22 11:53:31 -0800

    makes game restart

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,111 +1,163 @@
-var statusOfGame = true, winnah, wins=0, lose=0, totalScore=0, addingPoints, winningNumber
+var statusOfGame = true, wins=0, lose=0, totalScore=0, winningNumber
+
+//statusOfGame is true when points do not equal (in progress)
+//statusOfGame is false when points equal has been guessed (completed) or they lose the game
 
 var crystals = {
 	crystal1 : $("#crystal1"),
 	crystal2 : $("#crystal2"),
 	crystal3 : $("#crystal3"),
 	crystal4 : $("#crystal4")
-};
+}
 
-console.log(crystals);
-//statusOfGame is true when points do not equal (in progress)
-//statusOfGame is false when points equal has been guessed (completed) or they lose the game
 
-//generate random number for each crystal 
-	var randomNumber = function randomNumber() {
+var randomNumber = function randomNumber() {
 
-		statusOfGame = true;
+	statusOfGame = true;
 
-		for(let prop in crystals) {
+	for(let prop in crystals) {
 
-			crystals[prop] = Math.floor((Math.random() * 12) + 1);
-			console.log(typeof crystals[prop]);
-			console.log(crystals);
-		};
+		crystals[prop] = Math.floor((Math.random() * 12) + 1);
+		console.log(crystals);
 
-		//generate and add winning number to html 
-		winningNumber = Math.floor((Math.random() * 120) + 19);
-		console.log(winningNumber);
-		$("#randomNumber").html(winningNumber);
-		console.log("The total score is: " + totalScore + "the winning number is " + winningNumber)
 	};
 
+	winningNumber = Math.floor((Math.random() * 120) + 19);
 
-// $(".crystals").on("click", function() {
-// 	console.log("What is happening?")
-// 	console.log(totalScore);
-//when the crystals are clicked add their value to the variable totalScore and display
-		//add the value of each crystal to a total score
-var addingPoints = function addingPoints (){
+	$("#randomNumber").html(winningNumber);
+
+}
+
+
+var winnah = function winnah() {
+
+	wins += 1;
+	$("#wins").html("You've won this many times: " + wins);
+	statusOfGame = false;
+	confirm("YOU'VE WON!! Do you want to play again?");
+		if (true) {
+			restartGame();
+		} else {
+
+ 			alert("Bye Felicia!");
+
+ 		}
+
+}
+
+var loser = function loser() {
+
+	lose += 1;
+ 	$("#lose").html("You've lost this many times: " + lose);
+ 	statusOfGame = false;
+ 	var numberOff = totalScore - winningNumber;
+
+ 	confirm("Looks like you over guessed by " + numberOff + " points. Do you want to play again?");
+ 		if (true) {
+ 			restartGame();
+ 		} else {
+
+ 			alert("Bye Felicia!");
+
+ 		}
+
+}
+
+var restartGame = function restartGame() {
+
+	$("#randomNumber").empty();
+	$(".score").empty();
+	totalScore = 0;
+	randomNumber();
+	console.log('i am resetting' + totalScore)
+	console.log(crystals);
+
+}
+
+var addingPoints = function addingPoints() {
 
 	$("#crystal1").on("click", function() {
-	//display the total score
 		totalScore += crystals.crystal1;
-		console.log("the total score is : " + totalScore);
 		$(".score").html(totalScore);
 		console.log("The total score is: " + totalScore + "the winning number is " + winningNumber);
 		console.log("number of wins " + wins +" number of loses: " + lose);
+
+		if (winningNumber === totalScore) {
+
+			winnah();
+		} 
+
+		if (totalScore > winningNumber) {
+
+			loser();
+		}
 	});
+
 
 	$("#crystal2").on("click", function() {
-	//display the total score
 		totalScore += crystals.crystal2;
-		console.log("the total score is : " + totalScore);
 		$(".score").html(totalScore);
 		console.log("The total score is: " + totalScore + "the winning number is " + winningNumber);
 		console.log("number of wins " + wins +" number of loses: " + lose);
+
+		if (winningNumber === totalScore) {
+
+			winnah();
+		}
+
+		if (totalScore > winningNumber) {
+
+			loser();
+		}
+
 	});
+
 
 	$("#crystal3").on("click", function() {
-	//display the total score
 		totalScore += crystals.crystal3;
-		console.log("the total score is : " + totalScore);
 		$(".score").html(totalScore);
 		console.log("The total score is: " + totalScore + "the winning number is " + winningNumber);
 		console.log("number of wins " + wins +" number of loses: " + lose);
+
+		if (winningNumber === totalScore) {
+
+			winnah();
+		}
+
+		if (totalScore > winningNumber) {
+
+			loser();
+		}
 	});
 
+
 	$("#crystal4").on("click", function() {
-	//display the total score
 		totalScore += crystals.crystal4;
-		console.log("the total score is : " + totalScore);
 		$(".score").html(totalScore);
 		console.log("The total score is: " + totalScore + "the winning number is " + winningNumber);
 		console.log("number of wins " + wins +" number of loses: " + lose);
+
+		if (winningNumber === totalScore) {
+
+			winnah();
+		}
+
+		if (totalScore > winningNumber) {
+
+			loser();
+		}
 	});
+
+
 
 };	
 
-//function to "restart" the game when the win has been incremented
 
+//need function to "restart" the game when the win has been incremented
 /////////////////////////////////////////////////////////////////////////////////////////////
-
 if (statusOfGame = true) {
+
 	randomNumber();
 	addingPoints();
-
-	if (winningNumber === totalScore) {
-		console.log("Is this running?")
-		wins += 1;
-		$("#wins").html("You've won this many times: " + wins);
-		statusOfGame = false;
-
-	}
-
-	if (totalScore > winningNumber) {
-		
-		lose += 1;
-	 	$("#lose").html("You've lost this many times: " + lose);
-	 	statusOfGame = false;
-	}
 }
-// } else {
-
-// 	//restart the game
-// }
-
-
-
-
-
 
